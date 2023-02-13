f49b03ca4850d5c9256711c203f2f833e69faf73
giraffeyworks <92187780+giraffeyworks@users.noreply.github.com>
2023-02-13 10:08:09 -0500

    Update download.py

--- download.py
+++ download.py
@@ -9,13 +9,11 @@ import torch
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
     print("downloading model...")
-    GPTJForCausalLM.from_pretrained(
-        "EleutherAI/gpt-j-6B", revision="float16", torch_dtype=torch.float16, low_cpu_mem_usage=True
-    )
+    AutoModelForCausalLM.from_pretrained("PygmalionAI/pygmalion-6b")
     print("done")
 
     print("downloading tokenizer...")
-    GPT2Tokenizer.from_pretrained("EleutherAI/gpt-j-6B")
+    tokenizer = AutoTokenizer.from_pretrained("PygmalionAI/pygmalion-6b")
     print("done")
 
 if __name__ == "__main__":

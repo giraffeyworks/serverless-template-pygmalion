a756e3369690f8c6596ed3d157e72a6e59a7fbc2
giraffeyworks <92187780+giraffeyworks@users.noreply.github.com>
2023-02-13 10:06:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def init():
     global tokenizer
 
     print("loading to CPU...")
-    model = GPTJForCausalLM.from_pretrained("EleutherAI/gpt-j-6B", revision="float16", torch_dtype=torch.float16, low_cpu_mem_usage=True)
+    model = AutoModelForCausalLM.from_pretrained("PygmalionAI/pygmalion-6b")
     print("done")
 
     # conditionally load to GPU
@@ -19,7 +19,7 @@ def init():
         model.cuda()
         print("done")
 
-    tokenizer = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-j-6B")
+    tokenizer = AutoTokenizer.from_pretrained("PygmalionAI/pygmalion-6b")
 
 
 # Inference is ran for every server call

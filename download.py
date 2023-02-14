7684ed9d60feaaff09ffbab036efe307f7475033
giraffeyworks <92187780+giraffeyworks@users.noreply.github.com>
2023-02-14 17:53:19 -0500

    Update download.py

--- download.py
+++ download.py
@@ -9,11 +9,11 @@ import torch
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
     print("downloading model...")
-    AutoModelForCausalLM.from_pretrained("Pygmalion/pygmalion-350m")
+    AutoModelForCausalLM.from_pretrained("PygmalionAI/pygmalion-350m")
     print("done")
 
     print("downloading tokenizer...")
-    tokenizer = AutoTokenizer.from_pretrained("Pygmalion/pygmalion-350m")
+    tokenizer = AutoTokenizer.from_pretrained("PygmalionAI/pygmalion-350m")
     print("done")
 
 if __name__ == "__main__":
